3af898bb3d126a448da0a64462075f3763b4952c
Arul Jayaraman <arulelango.jayaraman@digital.justice.gov.uk>
2021-09-28 17:35:12 +0100

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,35 @@ configurations {
 }
 
 dependencies {
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+  // Spring boot dependencies
+  implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+  // GOVUK Notify:
+  implementation("uk.gov.service.notify:notifications-java-client:3.17.2-RELEASE")
+
+  // Database dependencies
+  runtimeOnly("com.h2database:h2")
+//  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.postgresql:postgresql:42.2.20")
+
+  implementation("com.google.code.gson:gson:2.8.8")
+  implementation("io.arrow-kt:arrow-core:0.10.5")
+
+  // OpenAPI
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.10")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
 }
 
 tasks {
