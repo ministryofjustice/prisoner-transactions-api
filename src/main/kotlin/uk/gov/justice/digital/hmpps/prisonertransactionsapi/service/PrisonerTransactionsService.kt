44195c82ec67b425c7b261ff623042c898ee4910
Arul Jayaraman <arulelango.jayaraman@digital.justice.gov.uk>
2021-09-30 14:38:13 +0100

    secret generation and email via gmail

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertransactionsapi/service/PrisonerTransactionsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertransactionsapi/service/PrisonerTransactionsService.kt
@@ -4,15 +4,23 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.prisonertransactionsapi.email.EmailSender
 import uk.gov.justice.digital.hmpps.prisonertransactionsapi.model.PrisonerTransactionsRequest
+import uk.gov.justice.digital.hmpps.prisonertransactionsapi.util.TokenGenerator
 
 @Service
-class PrisonerTransactionsService() {
-
+class PrisonerTransactionsService(
+  private val emailSender: EmailSender,
+  private val tokenGenerator: TokenGenerator
+) {
   val log: Logger = LoggerFactory.getLogger(this::class.java)
+
   @Transactional
   fun generateMagicLink(request: PrisonerTransactionsRequest) {
     log.info("generateMagicLink called....")
     log.info("Email id - " + request.email)
+    val secret = tokenGenerator.generateSecret()
+    log.info("Generated secret - $secret")
+    emailSender.sendEmail(request.email, secret)
   }
 }
