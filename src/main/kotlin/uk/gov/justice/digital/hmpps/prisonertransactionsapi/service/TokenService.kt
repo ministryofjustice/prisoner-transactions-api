cd12f3e0b0066cda4cfd79215bbb7e8330a33dcd
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-10-06 13:43:24 +0100

    314 Add logging to debug issue when deployed which can't be reproduced locally (#17)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertransactionsapi/service/TokenService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonertransactionsapi/service/TokenService.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.prisonertransactionsapi.service
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class TokenService {
+  val log: Logger = LoggerFactory.getLogger(this::class.java)
 
   // TODO move the token store to a RedisTokenStore
   private val tokenStore = mutableMapOf<String, String>()
@@ -14,7 +17,10 @@ class TokenService {
   }
 
   // TODO generate a JWT that includes the email and an expiry
-  fun generateToken(email: String) = generateSecret().also { tokenStore[it] = email }
+  fun generateToken(email: String) =
+    generateSecret()
+      .also { tokenStore[it] = email }
+      .also { log.info("Generated token $it for email $email") }
 
   fun getTokenEmail(token: String) = tokenStore.get(token)
 }
