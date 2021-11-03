40f33e5c2ea9cef0a28e9b8df09c04fa7d74ec5a
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2021-11-03 14:37:37 +0000

    Update dependencies (#36)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.12"
   kotlin("plugin.spring") version "1.5.31"
   kotlin("plugin.jpa") version "1.5.31"
 }
@@ -21,30 +21,30 @@ dependencies {
 
   // Database dependencies
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql:42.2.24")
+  runtimeOnly("org.postgresql:postgresql:42.3.1")
 
-  implementation("com.google.code.gson:gson:2.8.8")
+  implementation("com.google.code.gson:gson:2.8.9")
   implementation("io.jsonwebtoken:jjwt:0.9.1")
-  implementation("com.google.zxing:core:3.3.0")
-  implementation("com.google.zxing:javase:3.3.0")
+  implementation("com.google.zxing:core:3.4.1")
+  implementation("com.google.zxing:javase:3.4.1")
   implementation("net.sourceforge.barbecue:barbecue:1.5-beta1")
   implementation("net.sf.barcode4j:barcode4j:2.1")
 
   // OpenAPI
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.11")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.11")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.11")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.12")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.12")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.12")
 
   // Test dependencies
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
+  testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
   testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.28")
   testImplementation("org.mockito:mockito-inline:4.0.0")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("it.ozimov:embedded-redis:0.7.3")
-  testImplementation("org.testcontainers:postgresql:1.15.3")
+  testImplementation("org.testcontainers:postgresql:1.16.2")
 }
 
 java {
